27cf039ff95974665f463102805204cab8227f80
Carl0sSilva <Carl00sSilva@5e604d9b56f1>
2025-08-20 00:04:01 +0000

    Alteração aula 30

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,43 @@
 from flask import Flask, redirect, request, abort, make_response
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1><h2>Disciplina PTBDSWS</h2>'
+def home():
+    return """
+    <h1>Avaliação contínua: Aula 030</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/identificacao">Identificação</a></li>
+        <li><a href="/contexto">Contexto da Requisição</a></li>
+    </ul>
+    """
+
+@app.route('/identificacao')
+def identificacao():
+    return """
+    <h1>Avaliação contínua: Aula 030</h1>
+    <h2>Aluno: Carlos Custódio</h2>
+    <h2>Prontuário: PT3032043</h2>
+    <h2>Instituição: IFSP</h2>
+    <p>
+        <a href="/">Voltar</a>
+    </p>
+    """
+
+@app.route('/contexto')
+def contexto_requisicao():
+    user_agent = request.headers.get('User-Agent')
+    ip_remoto = request.remote_addr
+    host_app = request.host
+
+    return f"""
+    <h1>Avaliação contínua: Aula 030</h1>
+    <h2>Seu navegador é: {user_agent}</h2>
+    <h2>O IP do computador remoto é: {ip_remoto}</h2>
+    <h2>O host da aplicação é: {host_app}</h2>
+    <p>
+        <a href="/">Voltar</a>
+    </p>
+    """
 
 @app.route('/user/<name>')
 def user(name):
